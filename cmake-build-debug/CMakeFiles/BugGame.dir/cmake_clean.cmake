f5e6abdf704d836a5ba062111cbb774c45ab7d9b
All2be9s <2050821560@qq.com>
2022-03-10 23:01:50 +0800

    Create the project file.

---
+++ cmake-build-debug/CMakeFiles/BugGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BugGame.exe"
+  "BugGame.exe.manifest"
+  "BugGame.pdb"
+  "CMakeFiles/BugGame.dir/main.cpp.obj"
+  "libBugGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BugGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
